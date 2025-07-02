6d19b88e4779bbe45b151f861288a8eddff61430
Devansh <deevansh346@gmail.com>
2025-07-02 12:21:54 +0530

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -17,15 +17,4 @@
   })
 })();
 
-let taxSwitch = document.getElementById("switchCheckReverse");
-        taxSwitch.addEventListener("click", ()=>{
-            let taxInfo = document.getElementsByClassName("tax-info");
-            for(info of taxInfo){
-                if(info.style.display != "inline" )
-            {
-                info.style.display = "inline";
-            }
-            else {
-               info.style.display = "none";
-            }
-        }});
+ 
